654b0bb9d21143d9b4002d80458d7cf9a0d22354
DylanColeman <000914931@mohawkcollege.ca>
2024-09-24 19:56:23 -0400

    Added Camera Tracking
    Note, smooth rate might be a problem

---
+++ 2D Survivor/Assets/2D Survivor/Scripts/Camera Follow.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Camera will lock on to the specific target
+    public Transform target;
+
+    // Smooth movement variables
+    public float smoothRate = 1.5f;
+    public Vector3 offset = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Move the Camera towards the target (Player)
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Called once per frame after everything else has been updated
+    private void LateUpdate()
+    {
+        // Get the target position 
+        Vector3 targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        // Lerp means "linear interpolation" Learned it from Last Semester.
+        // Smoothly move the camera towards the target's position
+        // Time.deltatime is relative to the frame rate with the "smooth rate" to run it smoother.
+        transform.position = Vector3.Lerp(transform.position, targetPos + offset, Time.deltaTime * smoothRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- 2D Survivor/Assets/2D Survivor/Scripts/Movement.cs
+++ 2D Survivor/Assets/2D Survivor/Scripts/Movement.cs
@@ -13,37 +13,50 @@ public class Movement : MonoBehaviour
     // Variable for the Animator
     private Animator _anim;
 
-    // Varaible for SpriteRenderer
+    // Variable for SpriteRenderer
     private SpriteRenderer _sr;
 
     // Is the character moving
-    bool ismoving = false;
+    bool isMoving = false;
+
+    // Speed of movement
+    public float moveSpeed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Initalize Unity's Rigidbody2D
+        // Initialize Unity's Rigidbody2D
         _rb2d = GetComponent<Rigidbody2D>();
 
-        // Intialize the Animator
+        // Initialize the Animator
         _anim = GetComponent<Animator>();
 
         _sr = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per last frame
+    // FixedUpdate is called once per physics update
     private void FixedUpdate()
     {
-        
-    }
+        // Check for movement input
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
 
-    // Update is called once per frame
-    void Update()
-    {
-        ismoving = Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.W);
-        while (ismoving)
-        {
-            _anim.SetBool("isRunning", true);
-        }
+        // Set the movement
+        Vector2 movement = new Vector2(moveX, moveY);
+
+        // Move the player
+        _rb2d.velocity = movement * moveSpeed;
+
+        // Check if the player is moving
+        isMoving = movement.magnitude > 0;
+
+        // Update the animator
+        _anim.SetBool("isRunning", isMoving);
+
+        // Flip the sprite based on movement direction
+        if (moveX < 0)
+            _sr.flipX = true;
+        else if (moveX > 0)
+            _sr.flipX = false;
     }
 }
